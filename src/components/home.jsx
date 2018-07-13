cb9cb2b8618152febe2992efcc060c9e009c75d5
ndhaniff <ndhaniff@gmail.com>
2018-07-13 12:02:49 +0800

    initial commit

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Home extends Component{
+
+  render(){
+    return(
+      <div>
+        <h1>This is Home Component</h1>
+      </div>
+    )
+  }
+
+}
+
+export default Home
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  entry: ['./src/app.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react','es2015']
+          }
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react','es2015']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+}
